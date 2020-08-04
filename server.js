d5ab332954349ead2db9aa287c51052771fe498d
Gustavo Garcia <guscastles@gmail.com>
2020-08-04 11:13:55 +1000

    Updated server.js to handle app urls

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-const express = require('express')
+const express = require('express');
 const serveStatic = require("serve-static")
-const path = require('path')
-const fetch = require('node-fetch')
-const app = express()
+const path = require('path');
+const fetch = require('node-fetch');
+const app = express();
 
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 80;
-const DATA_URI = process.env.DATA_URI || 'https://eventqueue.herokuapp.com/events'
+const DATA_URI = process.env.DATA_URI || 'https://eventqueue.herokuapp.com/events';
+const HASH_URI = process.env.HASH_URI || 'https://eventqueue.herokuapp.com/hash';
 
 app.get('/events', (req, res) => {
-  console.log('Events requested')
-  console.log(`Data URI: ${DATA_URI}`)
-  fetch(DATA_URI, {method: 'GET'})
-    .then(response => response.json())
-    .then(json => {
-      res.send(json)
+  fetch(DATA_URI, {method: 'GET'}).
+    then(res => res.json()).
+    then((json) => {
+      res.send(json);
     });
 });
 
-app.listen(port)
-console.log("Server started at port " + port)
+app.get('/hash', (req, res) => {
+  fetch(`${HASH_URI}?previous=${req.query.previous}&data=${req.query.data}` , {method: 'GET'}).
+    then(res => res.json()).
+    then((json) => {
+      res.send(json);
+    });
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './dist', 'index.html'))
+});
+
+app.listen(port);
+console.log("Server started at port " + port);
