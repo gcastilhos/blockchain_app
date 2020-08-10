615f8377a394e301c71585ec95860ad0901f5597
Gustavo Garcia <guscastles@gmail.com>
2020-08-10 10:29:27 +1000

    Minor refactoring

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@
       <div class="col-12">
         <nav class="navbar navbar-expand">
           <div class="navbar-brand">Picogrid Blockchain</div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation">
             <span class="navbar-toggler-icon">Toggle Button</span>
           </button>
           <ul class="navbar-nav mr-auto">
